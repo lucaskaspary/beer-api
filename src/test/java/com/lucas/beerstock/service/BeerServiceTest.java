5cc5e4ebf68d54563ab6803136e12890d02f47ab
lucaskaspary <lucaskaspary@hotmail.com>
2022-01-20 17:34:06 -0300

    Developed beer API

---
+++ src/test/java/com/lucas/beerstock/service/BeerServiceTest.java
@@ -0,0 +1,23 @@
+package com.lucas.beerstock.service;
+
+import com.lucas.beerstock.mapper.BeerMapper;
+import com.lucas.beerstock.repository.BeerRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class BeerServiceTest {
+    private static final long INVALID_BEER_ID = 1L;
+
+    @Mock
+    private BeerRepository beerRepository;
+
+    private BeerMapper beerMapper = BeerMapper.INSTANCE;
+
+    @InjectMocks
+    private BeerService beerService;
+
+    
+}
